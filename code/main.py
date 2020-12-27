8793ac0654b2fb120796a0953d91f4ae374dcfe9
Vikas <44545660+vikasdo@users.noreply.github.com>
2020-12-27 15:13:31 +0000

    written logicforcreate

--- code/main.py
+++ code/main.py
@@ -1,3 +1,26 @@
+import sys,time
+class helperfunctions:
+	def check_dict_constraints(d):
+		if str(d.__sizeof__())>(1000*1024*1024):
+			print("The size of Dictionary is exceeded the limit 1GB")
+
+			return False
+
+	def check_key_constraints(key):
+		if len(key)>32 and key.isalpha() :
+			print("The size of key you entered is exceeded the limit OF 32 chars")
+			return False
+		
+
+
+	def check_val_constraints(val):
+		if sys.getsizeof(val)>(16*1024):
+			print("The size of val you entered is exceeded the limit OF 16 KB")
+
+			return false
+
+		
+
 class dbstore:
 
 	def __init__(self):
@@ -5,13 +28,35 @@ class dbstore:
 
 
 
-	def add(key,value,timeout=None):
-		pass
+	def add(key,value,timeout=0):
+		if key in self.data:
+			raise Exception("The Given key is created already..")
+		
+		try:
+			if helperfunctions.check_dict_constraints(self.data) and helperfunctions.check_key_constraints(key) and helperfunctions.check_val_constraints(value) :
+				if timeout==0:
+					self.data[key]=(value,timeout)
+				else:
+					self.data[key]=(value,time.time()+timeout)
+
+
+
+		except Exception as e:
+			print("The error is {}".format(e))
+		
+
 
 
 
 	def read(key):
-		pass
+		if key  in self.data():
+			print()
+
+
+
+		except:
+
+
 
 
 	def  delete(key):
