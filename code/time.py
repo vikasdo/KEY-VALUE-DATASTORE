cae991e8bd81b0bc299b4f12b44739d66e0f6e29
Vikas <44545660+vikasdo@users.noreply.github.com>
2020-12-28 15:20:24 +0000

    add

--- code/time.py
+++
@@ -1,5 +0,0 @@
-import time,sys
-#For Unix system, January 1, 1970, 00:00:00 at UTC is epoch (the point where time begins).
-# print(time.time())
-arr=[1,2,3,2,12111111111111111112]
-print(sys.getsizeof(arr))
